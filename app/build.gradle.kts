4c00c238ee807ef8c4eeb07b9283d1be53b8b39a
MayaTakenOutOfCode <mayawastaken12@gmail.com>
2024-10-23 12:14:25 +0200

    Task: Add new dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,7 @@ android {
 }
 
 dependencies {
-    //val navVersion = "2.8.3"
-
-    //implementation("androidx.navigation:navigation-compose:$navVersion")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
